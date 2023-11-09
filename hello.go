2d656b736c559cefd83d51b70c7eca751e08e66a
moises.assis <moises.assis@gympass.com>
2023-11-09 13:28:34 +0000

    feat: finished complete version of Hello World

--- hello.go
+++ hello.go
@@ -1,16 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-const englishHelloPrefix = "Hello, "
+// group all constants into single declaration
+const (
+	englishHelloPrefix    = "Hello, "
+	spanishHelloPrefix    = "Hola, "
+	frenchHelloPrefix     = "Bonjour, "
+	italianHelloPrefix    = "Buongiorno, "
+	portugueseHelloPrefix = "Olá, "
+)
 
-func Hello(name string) string {
+// public function: starts with a capital letter
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+// private function: starts with a lowercase letter
+func greetingPrefix(language string) (prefix string) {
+
+	switch language {
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	case "French":
+		prefix = frenchHelloPrefix
+	case "Italian":
+		prefix = italianHelloPrefix
+	case "Portuguese":
+		prefix = portugueseHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
+	// return the named return value "prefix"
+	// "prefix" initialized as empty and assigned inside func
 }
 
 func main() {
-	fmt.Println(Hello(""))
+	fmt.Println(Hello("Amélie", "French"))
+	fmt.Println(Hello("Michelangelo", "Italian"))
+	fmt.Println(Hello("Elizabeth", "English"))
 }
--- hello_test.go
+++ hello_test.go
@@ -2,11 +2,19 @@ package main
 
 import "testing"
 
+// if type is the same for multiple func parameters, just put type after the last one
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
 func TestHello(t *testing.T) {
 	t.Run(
 		"saying hello to people",
 		func(t *testing.T) {
-			got := Hello("Chris")
+			got := Hello("Chris", "")
 			want := "Hello, Chris"
 			assertCorrectMessage(t, got, want)
 
@@ -15,17 +23,40 @@ func TestHello(t *testing.T) {
 	t.Run(
 		"saying 'Hello, World' when an empty string is supplied",
 		func(t *testing.T) {
-			got := Hello("")
+			got := Hello("", "")
 			want := "Hello, World"
 			assertCorrectMessage(t, got, want)
 		})
 
-}
+	t.Run(
+		"in Spanish",
+		func(t *testing.T) {
+			got := Hello("Valentina", "Spanish")
+			want := "Hola, Valentina"
+			assertCorrectMessage(t, got, want)
+		})
 
-// if type is the same for multiple func parameters, just put type after the last one
-func assertCorrectMessage(t testing.TB, got, want string) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	t.Run(
+		"in French",
+		func(t *testing.T) {
+			got := Hello("Amélie", "French")
+			want := "Bonjour, Amélie"
+			assertCorrectMessage(t, got, want)
+		})
+
+	t.Run(
+		"in Italian",
+		func(t *testing.T) {
+			got := Hello("Giovanna", "Italian")
+			want := "Buongiorno, Giovanna"
+			assertCorrectMessage(t, got, want)
+		})
+
+	t.Run(
+		"in Portuguese",
+		func(t *testing.T) {
+			got := Hello("Milo", "Portuguese")
+			want := "Olá, Milo"
+			assertCorrectMessage(t, got, want)
+		})
 }
